2aa3b514421c54d5f86d01cb1fcc953cee4f9013
winchell459 <johncmorris@gmail.com>
2022-09-01 20:26:31 -0700

    end of class 9/1/22
    Added the TextInput puzzle and started the MorseCode puzzle

---
+++ Assets/Scripts/InputText.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputText : Puzzle
+{
+    public InputField inputField;
+    public Text inputText;
+    public Button button;
+
+    public string passcode = "12345";
+
+    public void ButtonClicked()
+    {
+        Debug.Log(inputText.text);
+        if(inputText.text == passcode)
+        {
+            inputField.interactable = false;
+            FindObjectOfType<GameHandler>().ObjectTriggered(this);
+        }
+    }
+}

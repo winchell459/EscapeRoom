40b8c4e6e3c21078dad80f8798721861828eb80d
winchell459 <johncmorris@gmail.com>
2023-02-17 22:34:41 -0800

    end of class 2/17/23

---
+++ Assets/Scripts/Networking/EscapeNetworkObjects.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeNetworkObjects : MonoBehaviour
+{
+    public List<GameObject> networkObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //add books to the networkObjects
+        if (FindObjectOfType<BookShelf>())
+        {
+            foreach(GameObject go in FindObjectOfType<BookShelf>().GetBooks())
+            {
+                networkObjects.Add(go);
+            }
+        }
+    }
+
+    public GameObject GetNetworkObject(int objectID)
+    {
+        if(objectID > 0 && objectID <= networkObjects.Count)
+        {
+            return networkObjects[objectID - 1];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public int GetNetworkObjectID(GameObject networkObject)
+    {
+        for(int i = 0; i < networkObjects.Count; i += 1)
+        {
+            if (networkObjects[i] == networkObject) return i + 1;
+        }
+        return 0;
+    }
+}
